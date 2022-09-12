de7ce52c9f3de5f13d962d7d37f3c1e9fccdc131
Marty <Marty2009@yandex.ru>
2022-09-13 00:29:48 +0300

    сделал моадлку заказа, настроил кнопки заказа на страничке сравнения

--- src/js/comparison.js
+++ src/js/comparison.js
@@ -1,12 +1,12 @@
 const comparisonCards = document.querySelectorAll('.comparison__card');
 
 function recalculatePropsHeights() {
-  const comparisonPropsHeights = new Array(comparisonCards[0].querySelectorAll('.camparison__characteristics-item').length).fill(null);
+  const comparisonPropsHeights = new Array(comparisonCards[0].querySelectorAll('.comparison__characteristics-item').length).fill(null);
   comparisonCards.forEach(comparisonCard => {
-    const characteristics = comparisonCard.querySelectorAll('.camparison__characteristics-item');
+    const characteristics = comparisonCard.querySelectorAll('.comparison__characteristics-item');
     characteristics.forEach((element, index) => {
       element.style.height = "";
-      element.classList.add('camparison__characteristics-item--mobile');
+      element.classList.add('comparison__characteristics-item--mobile');
       if (element.clientHeight > comparisonPropsHeights[index]) {
         comparisonPropsHeights[index] = element.clientHeight;
       }
@@ -14,10 +14,10 @@ function recalculatePropsHeights() {
   });
 
   comparisonCards.forEach(comparisonCard => {
-    const characteristics = comparisonCard.querySelectorAll('.camparison__characteristics-item');
+    const characteristics = comparisonCard.querySelectorAll('.comparison__characteristics-item');
     const mobileProps = document.querySelectorAll('.comparison__mobile-props-item');
     characteristics.forEach((element, index) => {
-      element.classList.remove('camparison__characteristics-item--mobile');
+      element.classList.remove('comparison__characteristics-item--mobile');
       if (comparisonPropsHeights[index] !== null) {
         element.style.height = comparisonPropsHeights[index] + "px";
         mobileProps[index].style.height = comparisonPropsHeights[index] + "px";
--- src/js/modal.js
+++ src/js/modal.js
@@ -1,5 +1,5 @@
 // Настройка модалок
-const modals = document.querySelectorAll('.modal');
+const modals = document.querySelectorAll('.modal:not(.modal--order');
 var modalMaskOptions = {
   mask: '+{7} (000) 000-00-00',
 };
@@ -95,5 +95,79 @@ const messengersChoices = new Choices(messengersSelect, {
     flippedState: 'is-flipped'
   }
 });
+
+const modalOrder = document.querySelector('.modal--order');
+if (modalOrder) {
+  const modalOrderMaskOptions = {
+    mask: '+{7} (000) 000-00-00',
+  };
+  const modalOrderPhone = document.querySelector('#modal-order-phone');
+  IMask(modalOrderPhone, modalOrderMaskOptions);
+
+  const modalOrderForm = document.querySelector('.modal--order .modal__form');
+  const modalOrderPristine = new Pristine(modalOrderForm, {
+    classTo: 'form__label', // Элемент, на который будут добавляться классы
+    errorTextParent: 'form__label', // Элемент, куда будет выводиться текст с ошибкой
+    errorTextTag: 'span', // Тег, который будет обрамлять текст ошибки
+    errorTextClass: 'form__error-message' // Класс для элемента с текстом ошибки
+  }, true);
+  modalOrderForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    var valid = modalOrderPristine.validate();
+    // TODO здесь должен написать запрос программист
+  });
+  modalOrder.querySelector('.modal__close-button').addEventListener('click', hideModal);
+
+   // Настраиваем селект
+   const modalOrderMessengersSelect = document.querySelector('#modal-order-messengers');
+   const modalOrderMessengersChoices = new Choices(modalOrderMessengersSelect, {
+     placeholder: false,
+     searchEnabled: false,
+     shouldSort: false,
+     itemSelectText: '',
+     classNames: {
+       containerOuter: 'form-choices',
+       containerInner: 'form-choices__inner',
+       input: 'form-choices__input',
+       inputCloned: 'form-choices__input--cloned',
+       list: 'form-choices__list',
+       listItems: 'form-choices__list--multiple',
+       listSingle: 'form-choices__list--single',
+       listDropdown: 'form-choices__list--dropdown',
+       item: 'form-choices__item',
+       itemSelectable: 'form-choices__item--selectable',
+       itemDisabled: 'form-choices__item--disabled',
+       itemOption: 'form-choices__item--choice',
+       group: 'choices__group',
+       groupHeading : 'choices__heading',
+       button: 'choices__button',
+       activeState: 'is-active',
+       focusState: 'is-focused',
+       openState: 'is-open',
+       disabledState: 'is-disabled',
+       highlightedState: 'is-highlighted',
+       selectedState: 'is-selected',
+       flippedState: 'is-flipped'
+     }
+   });
+
+    const orderButtons = document.querySelectorAll('.order-button');
+    orderButtons.forEach(orderButton => {
+      orderButton.addEventListener('click', (evt) => {
+        evt.preventDefault(); // Запрет перехода по ссылке
+        if (orderButton.classList.contains('comparison__card-button')) {
+          const productName = orderButton.closest('.comparison__card').
+                                          querySelector('.comparison__model').textContent;
+          const modalOrderProduct = modalOrder.querySelector('.modal__product-name');
+          modalOrderProduct.textContent = productName;
+          const modalOrderProductInput = modalOrder.querySelector('#modal-order-product');
+          modalOrderProductInput.value = productName;
+        }
+        modalOrder.classList.add('modal--opened');
+    })
+   });
+}
+
+
 
 
