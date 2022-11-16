eb509b5b9e8e66ca85f07fae143f773d67276f7c
Marty <Marty2009@yandex.ru>
2022-11-16 22:05:18 +0300

    сделал верохние фильтры в каталоге серверов, начал делать боковую панель в катлоге филтров

--- src/js/catalog.js
+++ src/js/catalog.js
@@ -6,6 +6,48 @@ catalogFilters.forEach(el => {
   })
 })
 
+// Настройка селектов
+
+const catalogSelects = document.querySelectorAll('#catalog-filters-form .form__select');
+
+catalogSelects.forEach(catalogSelect => {
+  const catalogSelectChoice = new Choices(catalogSelect, {
+    placeholder: true,
+    placeholderValue: null,
+    searchEnabled: false,
+    shouldSort: false,
+    itemSelectText: '',
+    classNames: {
+      containerOuter: 'form-choices form-choices--righted',
+      containerInner: 'form-choices__inner',
+      input: 'form-choices__input',
+      inputCloned: 'form-choices__input--cloned',
+      list: 'form-choices__list',
+      listItems: 'form-choices__list--multiple',
+      listSingle: 'form-choices__list--single',
+      listDropdown: 'form-choices__list--dropdown',
+      item: 'form-choices__item',
+      placeholder: 'form-choices__paceholder',
+      itemSelectable: 'form-choices__item--selectable',
+      itemDisabled: 'form-choices__item--disabled',
+      itemOption: 'form-choices__item--choice',
+      group: 'choices__group',
+      groupHeading : 'choices__heading',
+      button: 'choices__button',
+      activeState: 'is-active',
+      focusState: 'is-focused',
+      openState: 'is-open',
+      disabledState: 'is-disabled',
+      highlightedState: 'is-highlighted',
+      selectedState: 'is-selected',
+      flippedState: 'is-flipped'
+    },
+    allowHTML: true
+  });
+});
+
+// TODO: сделать сортировку через селект
+
 
 // ТЕСТ ФИЛЬТРОВ
 const productCardList = document.querySelector('.catalog__cards-list');
