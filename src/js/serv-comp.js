d15c89f4e7d42f4ee57c830191f94e8c1581bc99
Marty <Marty2009@yandex.ru>
2022-12-01 22:14:37 +0300

    доделал интерактив кнопочек

--- src/js/serv-comp.js
+++ src/js/serv-comp.js
@@ -1,4 +1,12 @@
-window.addEventListener('resize', showButtons);
+const servCompValuesCont = document.querySelector('.serv-comp__param-value-wrapper:last-child');
+const servCompButtons = document.querySelector('.serv-comp__buttons');
+const servCompNext = document.querySelector('#serv-comp-next');
+const servCompPrev = document.querySelector('#serv-comp-prev');
+const servCompContent = document.querySelector('.serv-comp__content');
+const firstServParamRow = document.querySelectorAll('.serv-comp__param-values')[0];
+
+const SERV_COMP_BIAS = 152;
+let servCompTrVal = 0;
 
 function addProductToCompare(num = 1) {
   const paramValues = document.querySelectorAll('.serv-comp__param-values');
@@ -10,66 +18,78 @@ function addProductToCompare(num = 1) {
     }
   });
 }
-addProductToCompare();
+addProductToCompare(5);
+showButtons();
 
-const servCompValuesCont = document.querySelector('.serv-comp__param-value-wrapper:last-child');
-const servCompButtons = document.querySelector('.serv-comp__buttons');
-const servCompNext = document.querySelector('#serv-comp-next');
-const servCompPrev = document.querySelector('#serv-comp-prev');
-const servCompContent = document.querySelector('.serv-comp__content');
-const firstServComp = document.querySelectorAll('.serv-comp__param-values')[0].querySelector('.serv-comp__param-value-wrapper:first-child');
-const lastServComp = document.querySelectorAll('.serv-comp__param-values')[0].querySelector('.serv-comp__param-value-wrapper:last-child');
-const documentWidth = document.documentElement.offsetWidth;
-console.log("first", firstServComp.getBoundingClientRect().left, firstServComp.getBoundingClientRect().right);
-console.log("last", lastServComp.getBoundingClientRect().left, lastServComp.getBoundingClientRect().right);
+window.addEventListener('resize', showButtons);
+
+if (!servCompButtons.classList.contains('serv-comp__buttons--hidden')) {
+  setServButtonsDisable(0);
+}
 
 function showButtons() {
-  const documentWidth = document.documentElement.offsetWidth;
-  const contWidth = servCompValuesCont.getBoundingClientRect();
-  console.log(documentWidth, contWidth, contWidth.left, contWidth.right);
-  if (contWidth.left < 0 || contWidth.right > documentWidth) {
-    console.log("OVERFLOW!!!");
-    // servCompButtons.classList.remove('serv-comp__buttons--hidden');
+  const contRect = servCompContent.getBoundingClientRect();
+  const firstServComp = firstServParamRow.querySelector('.serv-comp__param-value-wrapper:first-child');
+  const lastServComp = firstServParamRow.querySelector('.serv-comp__param-value-wrapper:last-child');
+
+  servCompTrVal = 0;
+  const paramValues = document.querySelectorAll('.serv-comp__param-values');
+  setServButtonsDisable(0);
+  paramValues.forEach(element => {
+    element.style.transform = `translateX(${servCompTrVal}px`;
+  });
+
+  if (firstServComp.getBoundingClientRect().left < contRect.left ||
+      lastServComp.getBoundingClientRect().right > contRect.right) {
+    servCompButtons.classList.remove('serv-comp__buttons--hidden');
   } else {
-    // servCompButtons.classList.add('serv-comp__buttons--hidden');
+    servCompButtons.classList.add('serv-comp__buttons--hidden');
   }
 }
 
-const SERV_COMP_BIAS = 152;
-let servCompTrVal = 0;
+
+
 servCompNext.addEventListener('click', () => {
-  if (!servCompNext.disabled) {
-    servCompTrVal -= SERV_COMP_BIAS;
-    const paramValues = document.querySelectorAll('.serv-comp__param-values');
-    const enableStateNextButton = willServCompContOverflowRight(-SERV_COMP_BIAS); // ok
-    const enableStatePrevButton = willServCompContOverflowLeft(-SERV_COMP_BIAS);
-    // const needEnablePrevButton = (firstServComp.getBoundingClientRect().left + 152 ) > 0;
-    console.log(lastServComp.getBoundingClientRect().right - 152, documentWidth);
-    paramValues.forEach(element => {
-      element.style.transform = `translateX(${servCompTrVal}px`;
-    });
-    servCompNext.disabled = enableStateNextButton;
-    servCompPrev.disabled = enableStatePrevButton;
-    servCompNext.classList.add('section__swiper-button--disabled');
-  }
+  servCompTrVal -= SERV_COMP_BIAS;
+  const paramValues = document.querySelectorAll('.serv-comp__param-values');
+  setServButtonsDisable(-SERV_COMP_BIAS);
+  paramValues.forEach(element => {
+    element.style.transform = `translateX(${servCompTrVal}px`;
+  });
 })
 servCompPrev.addEventListener('click', () => {
-  if (!servCompPrev.disabled) {
-    servCompTrVal += SERV_COMP_BIAS;
-    const paramValues = document.querySelectorAll('.serv-comp__param-values');
-    const enableStateNextButton = willServCompContOverflowRight(SERV_COMP_BIAS); // ok
-    const enableStatePrevButton = willServCompContOverflowLeft(SERV_COMP_BIAS);
-    paramValues.forEach(element => {
-      element.style.transform = `translateX(${servCompTrVal}px`;
-    });
-    servCompNext.disabled = enableStateNextButton;
-    servCompPrev.disabled = enableStatePrevButton;
-  }
+  servCompTrVal += SERV_COMP_BIAS;
+  const paramValues = document.querySelectorAll('.serv-comp__param-values');
+  setServButtonsDisable(SERV_COMP_BIAS);
+  paramValues.forEach(element => {
+    element.style.transform = `translateX(${servCompTrVal}px`;
+  });
 })
 
 function willServCompContOverflowRight(bias) {
-  return (lastServComp.getBoundingClientRect().right + bias) < documentWidth;
+  const lastServComp = firstServParamRow.querySelector('.serv-comp__param-value-wrapper:last-child');
+  const contRect = servCompContent.getBoundingClientRect();
+  return (lastServComp.getBoundingClientRect().right + bias) > contRect.right;
 }
 function willServCompContOverflowLeft(bias) {
-  return (firstServComp.getBoundingClientRect().left + bias) > 0;
+  const firstServComp = firstServParamRow.querySelector('.serv-comp__param-value-wrapper:first-child');
+  const contRect = servCompContent.getBoundingClientRect();
+  return (firstServComp.getBoundingClientRect().left + bias) < contRect.left;
+}
+function setServButtonsDisable(bias) {
+  const nextButtonDisabled = !willServCompContOverflowRight(bias);
+  const prevButtonDisabled = !willServCompContOverflowLeft(bias);
+  servCompNext.disabled = nextButtonDisabled;
+  servCompPrev.disabled = prevButtonDisabled;
+  if (servCompNext.disabled) {
+    servCompNext.classList.add('section__swiper-button--lock');
+  } else {
+    servCompNext.classList.remove('section__swiper-button--lock');
+  }
+
+  if (servCompPrev.disabled) {
+    servCompPrev.classList.add('section__swiper-button--lock');
+  } else {
+    servCompPrev.classList.remove('section__swiper-button--lock');
+  }
 }
