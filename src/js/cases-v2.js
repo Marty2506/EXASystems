59bb71aad5475f989ed0f48eee1333deee827016
Marty <Marty2009@yandex.ru>
2023-03-30 22:41:16 +0300

    сделал спинер и добавил его на страницу кейсов

--- src/js/cases-v2.js
+++ src/js/cases-v2.js
@@ -1,7 +1,9 @@
 const caseCards = document.querySelectorAll(".cases__card");
 const MAX_CARDS_TEST = 10;
+const casesSpinner = document.querySelector("#cases-spinner");
 
 caseCards.forEach(initCard);
+hideSpinner();
 
 function initCard(el) {
   const previews = el.querySelectorAll("[data-case-card-preview]");
@@ -28,8 +30,9 @@ if (caseCards) {
   const page = document.querySelector(".page");
   const cardList = document.querySelector(".cases__list");
   let lastScrollTop = 0;
+  let block = false;
   document.addEventListener("scroll", () => {
-    // console.log(pageMain.scrollTop);
+    if (block) return;
     if (page.scrollTop < lastScrollTop) {
       // upscroll
       return;
@@ -39,19 +42,35 @@ if (caseCards) {
         element.offsetTop + element.offsetHeight - page.offsetHeight &&
       document.querySelectorAll(".cases__card").length < MAX_CARDS_TEST
     ) {
-      // const loading = document.createElement("h1");
-      // loading.textContent = "Загрузка";
-      // cardList.appendChild(loading);
-
-      console.log("End");
-      const newCard = document.querySelector(".cases__card").cloneNode(true);
-      initCard(newCard);
-      const newCardBigImage = newCard.querySelector(
-        "[data-case-card-original]"
-      );
-      newCardBigImage.addEventListener("click", addImageModalOpener);
-      cardList.appendChild(newCard);
+      block = true;
+      showSpinner();
+      fakeFetch(1000).then(() => {
+        const newCard = document.querySelector(".cases__card").cloneNode(true);
+        initCard(newCard);
+        const newCardBigImage = newCard.querySelector(
+          "[data-case-card-original]"
+        );
+        newCardBigImage.addEventListener("click", addImageModalOpener);
+        cardList.appendChild(newCard);
+        hideSpinner();
+        block = false;
+      });
     }
     lastScrollTop = page.scrollTop;
   });
+}
+
+// Функция для имитации запроса к серверу
+// Нужно будет в реальности вставить нормальный fetch и отрисовать карточки
+// Кто будет этим заниматься - можете написать мне и я сделаю подгрузку и отрисовку
+// Мне нужно знать в каком виде хранится на сервере. Это поля или html карточка целиком
+function fakeFetch(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+function showSpinner() {
+  casesSpinner.classList.remove("spinner--hidden");
+}
+function hideSpinner() {
+  casesSpinner.classList.add("spinner--hidden");
 }
