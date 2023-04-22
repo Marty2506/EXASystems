6d8a1887923fc6b6214218eb5af0dd8d1b285a98
Marty <Marty2009@yandex.ru>
2023-04-22 22:31:11 +0300

    доделал мобильное меню

--- src/js/main.js
+++ src/js/main.js
@@ -1,42 +1,38 @@
-const header = document.querySelector('.page-header');
-
-window.addEventListener('scroll', (evt) => {
-  if (document.body.scrollTop > SCROLL_SIZE || document.documentElement.scrollTop > SCROLL_SIZE) {
-    header.classList.add('page-header--sticky');
-  } else {
-    header.classList.remove('page-header--sticky');
-  }
-})
-
-const numberInputsWithArrows = document.querySelectorAll('.form__field-number-wrapper input[type="number"]');
-numberInputsWithArrows.forEach(element => {
+const numberInputsWithArrows = document.querySelectorAll(
+  '.form__field-number-wrapper input[type="number"]'
+);
+numberInputsWithArrows.forEach((element) => {
   const mask = IMask(element, {
     mask: Number,
     scale: 0,
     signed: false,
     min: element.min,
-    max: element.max
+    max: element.max,
   });
 
-  element.addEventListener('blur', () => {
+  element.addEventListener("blur", () => {
     if (element.value === "") {
       element.value = 0;
       mask.updateValue();
     }
-  })
-  const incButton = element.parentNode.querySelector('.form__number-input-button--inc');
+  });
+  const incButton = element.parentNode.querySelector(
+    ".form__number-input-button--inc"
+  );
   if (incButton) {
-    incButton.addEventListener('click', () => {
+    incButton.addEventListener("click", () => {
       element.stepUp();
       mask.updateValue();
-    })
+    });
   }
-  const decButton = element.parentNode.querySelector('.form__number-input-button--dec');
+  const decButton = element.parentNode.querySelector(
+    ".form__number-input-button--dec"
+  );
   if (decButton) {
-    decButton.addEventListener('click', () => {
+    decButton.addEventListener("click", () => {
       element.stepDown();
       mask.updateValue();
-    })
+    });
   }
   // element.parentNode.parentNode.addEventListener('mousedown', (evt) => {
   //   evt.preventDefault();
--- src/js/menu.js
+++ src/js/menu.js
@@ -1,75 +1,93 @@
-const menu = document.querySelector('.menu');
-const menuSubs = menu.querySelectorAll('.menu__sub');
+const menu = document.querySelector(".menu");
+const mainMenu = document.querySelector(".menu__main");
+const menuSubs = menu.querySelectorAll(".menu__sub");
 
-menu.classList.remove('menu--no-js');
+menu.classList.remove("menu--no-js");
 
-menuSubs.forEach(submenu => {
-  const link = submenu.parentNode.querySelector('.menu__link');
+menuSubs.forEach((submenu) => {
+  const link = submenu.parentNode.querySelector(".menu__link");
   if (link) {
-    link.addEventListener('click', (evt) => {
-      if (!link.classList.contains('menu__link--end') && window.innerWidth < 1181) {
+    link.addEventListener("click", (evt) => {
+      if (
+        !link.classList.contains("menu__link--end") &&
+        window.innerWidth < 1181
+      ) {
         evt.preventDefault();
-        submenu.classList.add('menu__sub--active');
-        const backButton = submenu.querySelector('.menu__back-button');
-        backButton.addEventListener('click', () => {
-          submenu.classList.remove('menu__sub--active');
+        submenu.classList.add("menu__sub--active");
+        const backButton = submenu.querySelector(".menu__back-button");
+        backButton.addEventListener("click", () => {
+          submenu.classList.remove("menu__sub--active");
         });
       }
     });
   }
 });
 
-document.addEventListener('touchstart', handleTouchStart, false);
-document.addEventListener('touchmove', handleTouchMove, false);
+document.addEventListener("touchstart", handleTouchStart, false);
+document.addEventListener("touchmove", handleTouchMove, false);
 
 var xDown = null;
 var yDown = null;
 
 function getTouches(evt) {
-  return evt.touches ||             // browser API
-         evt.originalEvent.touches; // jQuery
+  return (
+    evt.touches || // browser API
+    evt.originalEvent.touches
+  ); // jQuery
 }
 
 function handleTouchStart(evt) {
-    const firstTouch = getTouches(evt)[0];
-    xDown = firstTouch.clientX;
-    yDown = firstTouch.clientY;
-};
+  const firstTouch = getTouches(evt)[0];
+  xDown = firstTouch.clientX;
+  yDown = firstTouch.clientY;
+}
 
 function handleTouchMove(evt) {
-    if ( ! xDown || ! yDown ) {
-        return;
-    }
+  if (!xDown || !yDown) {
+    return;
+  }
 
-    var xUp = evt.touches[0].clientX;
-    var yUp = evt.touches[0].clientY;
+  var xUp = evt.touches[0].clientX;
+  var yUp = evt.touches[0].clientY;
 
-    var xDiff = xDown - xUp;
-    var yDiff = yDown - yUp;
+  var xDiff = xDown - xUp;
+  var yDiff = yDown - yUp;
 
-    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
-        if ( xDiff > 0 ) {
-            /* right swipe */
-        } else {
-          /* left swipe */
-          const menuSub = evt.target.closest('.menu__sub');
-          if (menuSub) {
-            menuSub.querySelector('.menu__back-button').click();
-          } else {
-            const menu = evt.target.closest('.menu');
-            if (menu) {
-              burger.click();
-            }
-          }
-        }
+  if (Math.abs(xDiff) > Math.abs(yDiff)) {
+    /*most significant*/
+    if (xDiff > 0) {
+      /* right swipe */
     } else {
-        if ( yDiff > 0 ) {
-            /* down swipe */
-        } else {
-            /* up swipe */
+      /* left swipe */
+      const menuSub = evt.target.closest(".menu__sub");
+      if (menuSub) {
+        menuSub.querySelector(".menu__back-button").click();
+      } else {
+        const menu = evt.target.closest(".menu");
+        if (menu) {
+          burger.click();
         }
+      }
     }
-    /* reset values */
-    xDown = null;
-    yDown = null;
-};
+  } else {
+    if (yDiff > 0) {
+      /* down swipe */
+    } else {
+      /* up swipe */
+    }
+  }
+  /* reset values */
+  xDown = null;
+  yDown = null;
+}
+
+window.addEventListener("scroll", (evt) => {
+  if (
+    document.body.scrollTop > SCROLL_SIZE ||
+    document.documentElement.scrollTop > SCROLL_SIZE
+  ) {
+    mainMenu.classList.add("menu__main--sticky");
+  } else {
+    mainMenu.classList.remove("menu__main--sticky");
+  }
+});
