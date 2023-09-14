81ccb9ff861268e72d21b07a67da31807c652398
Marty <Marty2009@yandex.ru>
2023-09-14 20:18:05 +0300

    сделал моадлку консультации

--- src/js/modal.js
+++ src/js/modal.js
@@ -83,3 +83,85 @@ if (modalResume) {
     });
   });
 }
+
+const modalCons = document.querySelector(".modal--consultation");
+
+if (modalCons) {
+  const form = document.querySelector("#modal-cons-form");
+  const phoneEl = document.querySelector("#modal-cons-phone");
+
+  const modaConsPristine = new Pristine(
+    form,
+    {
+      classTo: "form__label", // Элемент, на который будут добавляться классы
+      errorTextParent: "form__label", // Элемент, куда будет выводиться текст с ошибкой
+      errorTextTag: "span", // Тег, который будет обрамлять текст ошибки
+      errorTextClass: "form__error-message", // Класс для элемента с текстом ошибки
+    },
+    true
+  );
+
+  modaConsPristine.addValidator(
+    phoneEl,
+    (value) => {
+      return value.length === 18;
+    },
+    "Номер неполный",
+    2,
+    false
+  );
+
+  // Настраиваем селект
+  const select = document.querySelector("#modal-cons-messengers");
+  const choices = new Choices(select, {
+    placeholder: false,
+    searchEnabled: false,
+    shouldSort: false,
+    itemSelectText: "",
+    classNames: {
+      containerOuter: "form-choices",
+      containerInner: "form-choices__inner",
+      input: "form-choices__input",
+      inputCloned: "form-choices__input--cloned",
+      list: "form-choices__list",
+      listItems: "form-choices__list--multiple",
+      listSingle: "form-choices__list--single",
+      listDropdown: "form-choices__list--dropdown",
+      item: "form-choices__item",
+      itemSelectable: "form-choices__item--selectable",
+      itemDisabled: "form-choices__item--disabled",
+      itemOption: "form-choices__item--choice",
+      group: "choices__group",
+      groupHeading: "choices__heading",
+      button: "choices__button",
+      activeState: "is-active",
+      focusState: "is-focused",
+      openState: "is-open",
+      disabledState: "is-disabled",
+      highlightedState: "is-highlighted",
+      selectedState: "is-selected",
+      flippedState: "is-flipped",
+    },
+    allowHTML: true,
+  });
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    var valid = modaConsPristine.validate();
+    // TODO здесь должен написать запрос программист
+  });
+  modalCons
+    .querySelector(".modal__close-button")
+    .addEventListener("click", hideModal);
+
+  const openConsModalButtons = document.querySelectorAll(
+    '[data-button-type="open-cons-modal"]'
+  );
+
+  openConsModalButtons.forEach((element) => {
+    element.addEventListener("click", (evt) => {
+      evt.preventDefault();
+      modalCons.classList.add("modal--opened");
+    });
+  });
+}
